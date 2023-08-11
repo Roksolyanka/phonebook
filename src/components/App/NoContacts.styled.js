8207dc6a6a591817269eccdbde7e036ad2aebf9f
Roksolyanka <roksolyanakushnir@gmail.com>
2023-08-11 20:27:35 +0300

    first part

---
+++ src/components/App/NoContacts.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const NoContacts = styled.p`
+  font-family: 'Roboto';
+  font-size: 25px;
+  font-weight: bold;
+  color: #282e09;
+  text-align: center;
+  margin-top: 10px;
+`;
---
+++ src/components/Filter/InputFind.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const InputFind = styled.input`
+  width: 320px;
+  height: 40px;
+  margin-left: 10px;
+  vertical-align: top;
+  font-size: 20px;
+  font-weight: bold;
+  background-color: #e2e2e2;
+
+  &: hover, focus {
+    box-shadow: #ffe724 0px 10px 20px, #ffe724 0px 2px 5px;
+  }
+`;
