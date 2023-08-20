93c6f04b90c536aba6ce5559dd7f30ae1c23d143
Roksolyanka <roksolyanakushnir@gmail.com>
2023-08-20 23:40:27 +0300

    finish

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import { addContactsThunk } from 'redux/contactsOperations';
 import { selectUserContacts } from 'redux/selectors';
@@ -13,8 +14,8 @@ export const ContactForm = () => {
   const contacts = useSelector(selectUserContacts);
   const dispatch = useDispatch();
 
- const [contactName, setContactName] = useState('');
- const [contactNumber, setContactNumber] = useState('');
+  const [contactName, setContactName] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -25,10 +26,7 @@ export const ContactForm = () => {
     const number = form.elements.contactNumber.value.trim();
 
     if (contacts.some(contact => contact.name === name))
-      return Notiflix.Notify.info(
-        `Contact with name ${name} already exists!`
-      );
-    
+      return Notiflix.Notify.info(`Contact with name ${name} already exists!`);
 
     if (contacts.some(contact => contact.number === number))
       return Notiflix.Notify.info(
@@ -39,31 +37,37 @@ export const ContactForm = () => {
 
     setContactName('');
     setContactNumber('');
+
+    Notiflix.Notify.success(`Contact ${name} successfully added.`);
   };
 
   return (
-      <ContainerForm>
-        <Form onSubmit={handleSubmit}>
-          <label>Name: </label>
-          <Input
-            type="text"
-            name="contactName"
-            value={contactName}
-            onChange={e => setContactName(e.target.value)}
-            required
-          />
+    <ContainerForm>
+      <Form onSubmit={handleSubmit}>
+        <label>Name: </label>
+        <Input
+          type="text"
+          name="contactName"
+          value={contactName}
+          onChange={e => setContactName(e.target.value)}
+          required
+        />
 
-          <label>Number: </label>
-          <Input
-            type="text"
-            name="contactNumber"
-            value={contactNumber}
-            onChange={e => setContactNumber(e.target.value)}
-            required
-          />
+        <label>Number: </label>
+        <Input
+          type="text"
+          name="contactNumber"
+          value={contactNumber}
+          onChange={e => setContactNumber(e.target.value)}
+          required
+        />
 
-          <Button type="submit">Add contact</Button>
-        </Form>
-      </ContainerForm>
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </ContainerForm>
   );
+};
+
+ContactForm.propTypes = {
+  contacts: PropTypes.array.isRequired,
 };
