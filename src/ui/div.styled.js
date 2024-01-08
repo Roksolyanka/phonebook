2cf91372ba8fbae5070f8d20e2879dd93ff00265
Roksolyanka <roksolyanakushnir@gmail.com>
2024-01-08 17:26:32 +0200

    corrected readme

---
+++ src/ui/div.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+// export const Box = styled.div`
+// ${space}
+// ${width}
+// ${color}
+// ${border}
+// ${layout}
+// ${shadow}
+// ${flexbox}
+// ${position}
+// ${background}
+// ${typography}
+
+// resize: ${({ resize }: Object) => resize};
+// cursor: ${({ cursor }: Object) => cursor};
+// opacity: ${({ opacity }: Object) => opacity};
+// transform: ${({ transform }: Object) => transform};
+// animation: ${({ animation }: Object) => animation};
+// word-break: ${({ wordBreak }: Object) => wordBreak};
+// transition: ${({ transition }: Object) => transition};
+// visibility: ${({ visibility }: Object) => visibility};
+// white-space: ${({ whiteSpace }: Object) => whiteSpace};
+// user-select: ${({ userSelect }: Object) => userSelect};
+// vertical-align: ${({ verticalAlign }: Object) => verticalAlign};
+// text-transform: ${({ textTransform }: Object) => textTransform};
+// text-decoration: ${({ textDecoration }: Object) => textDecoration};
+// `;
