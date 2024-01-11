478c5527fa389fad8372a348c52efbd9865f2cd6
Roksolyanka <roksolyanakushnir@gmail.com>
2024-01-11 17:45:35 +0200

    create component RoutesList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,45 +1,22 @@
-import { Suspense, lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
 
 import { Loader } from 'components/Loader/Loader';
-import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
 
 import { Header } from 'components/Header/Header';
 import { Footer } from 'components/Footer/Footer';
+import { RoutesList } from 'components/Routes/Routes';
 
-const HomePage = lazy(() => import('../../pages/HomePage/Home'));
-const ContactsPage = lazy(() => import('../../pages/ContactsPage/Contacts'));
-const RegisterPage = lazy(() => import('../../pages/RegisterPage/Register'));
-const LoginPage = lazy(() => import('../../pages/LoginPage/Login'));
-
-const routes = [
-  { path: '/', element: <HomePage /> },
-  {
-    path: '/contacts',
-    element: (
-      <PrivateRoute redirectTo="/login">
-        <ContactsPage />
-      </PrivateRoute>
-    ),
-  },
-  { path: '/login', element: <LoginPage /> },
-  { path: '/register', element: <RegisterPage /> },
-];
 
 export const App = () => {
   return (
     <div id="app-container">
-      <Header></Header>
+      <Header />
       <main>
         <Suspense fallback={<Loader />}>
-          <Routes>
-            {routes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-            ))}
-          </Routes>
+          <RoutesList />
         </Suspense>
       </main>
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 };
