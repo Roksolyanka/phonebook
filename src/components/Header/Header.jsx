e6e38cc39b797579d281216bb17188cbdba83802
Roksolyanka <roksolyanakushnir@gmail.com>
2024-01-11 15:44:02 +0200

    took away Home for authorized users

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,19 @@
-import { logoutUserThunk, refreshUserThunk } from "redux/operations";
-import { ButtonLogOut, HeaderStyle, NavLeft, NavRight, StyledNavLink, Welcome } from "./Header.styled";
-import { useDispatch, useSelector } from "react-redux";
-import { selectAuthentificated, selectToken, selectUserName } from "redux/selectors";
-import { useEffect } from "react";
+import { logoutUserThunk, refreshUserThunk } from 'redux/operations';
+import {
+  ButtonLogOut,
+  HeaderStyle,
+  NavLeft,
+  NavRight,
+  StyledNavLink,
+  Welcome,
+} from './Header.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectAuthentificated,
+  selectToken,
+  selectUserName,
+} from 'redux/selectors';
+import { useEffect } from 'react';
 
 export const Header = () => {
   const dispatch = useDispatch();
@@ -20,30 +31,27 @@ export const Header = () => {
   };
 
   return (
-      <HeaderStyle>
-        <NavLeft>
-          {authentificated ? (
-            <>
-              <StyledNavLink to="/">Home</StyledNavLink>
-              <StyledNavLink to="/contacts">Contacts</StyledNavLink>
-            </>
-          ) : (
-            <StyledNavLink to="/">Home</StyledNavLink>
-          )}
-        </NavLeft>
-        <NavRight>
-          {authentificated ? (
-            <>
-              <Welcome>Hello, {userName}!</Welcome>
-              <ButtonLogOut onClick={handleLogOut}>Log out</ButtonLogOut>
-            </>
-          ) : (
-            <>
-              <StyledNavLink to="/login">Login</StyledNavLink>
-              <StyledNavLink to="/register">Register</StyledNavLink>
-            </>
-          )}
-        </NavRight>
-      </HeaderStyle>
+    <HeaderStyle>
+      <NavLeft>
+        {authentificated ? (
+          <StyledNavLink to="/contacts">Contacts</StyledNavLink>
+        ) : (
+          <StyledNavLink to="/">Home</StyledNavLink>
+        )}
+      </NavLeft>
+      <NavRight>
+        {authentificated ? (
+          <>
+            <Welcome>Hello, {userName}!</Welcome>
+            <ButtonLogOut onClick={handleLogOut}>Log out</ButtonLogOut>
+          </>
+        ) : (
+          <>
+            <StyledNavLink to="/login">Login</StyledNavLink>
+            <StyledNavLink to="/register">Register</StyledNavLink>
+          </>
+        )}
+      </NavRight>
+    </HeaderStyle>
   );
 };
