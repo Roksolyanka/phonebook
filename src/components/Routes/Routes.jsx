5bbcd386fd56134e585ba866306a9d4569836f86
Roksolyanka <roksolyanakushnir@gmail.com>
2024-01-11 17:45:35 +0200

    create component RoutesList

---
+++ src/components/Routes/Routes.jsx
@@ -0,0 +1,32 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
+
+const HomePage = lazy(() => import('../../pages/HomePage/Home'));
+const ContactsPage = lazy(() => import('../../pages/ContactsPage/Contacts'));
+const RegisterPage = lazy(() => import('../../pages/RegisterPage/Register'));
+const LoginPage = lazy(() => import('../../pages/LoginPage/Login'));
+
+const routes = [
+  { path: '/', element: <HomePage /> },
+  {
+    path: '/contacts',
+    element: (
+      <PrivateRoute redirectTo="/login">
+        <ContactsPage />
+      </PrivateRoute>
+    ),
+  },
+  { path: '/login', element: <LoginPage /> },
+  { path: '/register', element: <RegisterPage /> },
+];
+
+export const RoutesList = () => {
+  return (
+    <Routes>
+      {routes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+    </Routes>
+  );
+};
