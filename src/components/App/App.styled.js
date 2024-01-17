c0c51877fc10dd3e3463ee324ccffe99f0390e19
Roksolyanka <roksolyanakushnir@gmail.com>
2024-01-17 19:08:07 +0200

    create button edit

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -6,9 +6,9 @@ export const TitlePhonebook = styled.h1`
   width: 100%;
   color: ${({ theme }) => theme.colors.secondaryColor};
   text-align: center;
-text - shadow: ${({ theme }) => theme.colors.textShadowColor} 10px 0 5px;
+  text-shadow: ${({ theme }) => theme.colors.textShadowColor} 10px 0 5px;
   margin-bottom: 10px;
   margin-top: 10px;
-box - shadow: ${({ theme }) => theme.colors.accentColor} 0px 20px 30px,
-  ${({ theme }) => theme.colors.accentColor} 0px 2px 5px;
+  box-shadow: ${({ theme }) => theme.colors.accentColor} 0px 20px 30px,
+    ${({ theme }) => theme.colors.accentColor} 0px 2px 5px;
 `;
